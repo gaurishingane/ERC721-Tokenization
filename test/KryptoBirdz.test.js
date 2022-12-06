d173d29ac35913d52f6e55a86106b886f048b59f
gaurishingane <shinganegauri28@gmail.com>
2022-12-06 18:06:10 +0530

    Test files added

--- test/KryptoBirdz.test.js
+++ test/KryptoBirdz.test.js
@@ -5,16 +5,76 @@ require("chai")
 .use(require("chai-as-promised"))
 .should()
 
-describe('tests deployment', async () => {
+contract('KryptoBirdz', (accounts) => {
     let contract
 
-    it('deploys smart contracts', async () => {
+    before( async() => {
         contract = await kryptoBirdz.deployed()
-        const address = contract.address
+    })
 
-        assert.notEqual(address, '')
-        assert.notEqual(address, null)
-        assert.notEqual(address, undefined)
-        assert.notEqual(address, 0x0)
+    describe('tests deployment', async () => {
+            
+        it('deploys smart contracts', async () => {
+            
+            const address = await contract.address
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+            assert.notEqual(address, 0x0)
+        })
+    
+        it('verifies metadata name', async () => {
+            const name = await contract.getName()
+    
+            assert.equal(name, 'Kryptobirdz')
+        })
+    
+        it('verifies symbol', async () => {
+            const symbol = await contract.getSymbol()
+    
+            assert.equal(symbol, 'KBIRDZ')
+        })
+    })
+
+    describe('Mints tokens', async() => {
+
+        it('mints a token', async() => { 
+            const token = await contract.mint('http...1')
+            const totalSupply = await contract.totalSupply()
+            assert.equal(totalSupply.words[0], 1)
+
+
+            const eventData = await token.logs[0].args
+            assert.equal(eventData._from,'0x0000000000000000000000000000000000000000','from is the contract')
+            assert.equal(eventData._to, accounts[0],'To is the msg.sender')
+
+            await contract.mint('http...1').should.be.rejected                  
+        })
+    })
+
+
+    describe('indexing of tokens', async() => {
+
+        it('fetches all minted tokens', async() => {
+
+            await contract.mint('http...2')
+            await contract.mint('http...3')
+            await contract.mint('http...4')
+
+            let totalSupply = await contract.totalSupply()
+
+            let result = []
+            let kBird
+
+            for(i = 0; i < totalSupply.words[0]; i++) {
+                kBird = await contract.kryptoBirdz(i)
+                result.push(kBird)
+            }
+
+            let expected = ['http...1','http...2','http...3','http...4']
+
+            assert.equal(expected.join(','), result.join(','))
+        })
     })
 })
+
